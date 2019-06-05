3f68d6008146a88d3fd50cae1adf9923c66ddab1
stevie cutman <steviecut@hotmail.fr>
2019-06-05 15:33:05 +0200

    event controller et routes update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :participations
+  has_many :participation
+  has_many :event, through: :participation
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :photo, PhotoUploader
