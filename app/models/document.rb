33e82eddf6981372d8e3ede1de90be46a8a07327
Daniel Kaddous <danielkaddous.ys@gmail.com>
2019-06-06 12:44:27 +0200

    Unbugged

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,5 @@
 class Document < ApplicationRecord
   belongs_to :task
   belongs_to :participation
-<<<<<<< HEAD
   belongs_to :user
-=======
-
->>>>>>> f377ebbde8bae252edfd35489760f9121bf1551d
 end
