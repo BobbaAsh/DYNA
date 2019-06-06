193cf53341f640adb912a8432b25da06f4887f9b
Daniel Kaddous <danielkaddous.ys@gmail.com>
2019-06-06 15:35:38 +0200

    Volunteers

--- db/migrate/20190606110221_add_columns_to_participations.rb
+++ db/migrate/20190606110221_add_columns_to_participations.rb
@@ -4,4 +4,5 @@ class AddColumnsToParticipations < ActiveRecord::Migration[5.2]
     add_column :participations, :event_id, :string
     add_column :participations, :wanted_status, :string
     add_column :participations, :status, :boolean
+  end
 end
