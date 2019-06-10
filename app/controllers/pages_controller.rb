dd53e1081cec001d7566566706597453a72d1bb9
Joachim Bourquardez <j.bourquardez@laposte.net>
2019-06-10 14:30:58 +0200

    la liste des documents dans la landing de l'user benevol fonctionne

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     # @participations = Participation.last(3)
     @participations = current_user.participations.last(3)
+    @documents = Document.all
   end
 
 end
