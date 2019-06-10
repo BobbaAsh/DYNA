2fc66f2fa5c6b349088e8aa4b9a5abc708a40803
Joachim Bourquardez <j.bourquardez@laposte.net>
2019-06-10 12:17:34 +0200

    affichage des 3 dernieres participations du current user benevol dans home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @participations = Participation.last(3)
+    # @participations = Participation.last(3)
+    @participations = current_user.participations.last(3)
   end
+
 end
