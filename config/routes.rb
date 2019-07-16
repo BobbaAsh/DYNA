efcfbbae209e790141718293de0fb3fd04d2c9c8
Chloe Le Gall <chloe.legall7@gmail.com>
2019-07-16 16:25:22 +0200

    prestataire

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :prestataires, path: 'pr'
   # get 'status/new'
   # get 'status/create'
   # get 'status/delete'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_135923) do
+ActiveRecord::Schema.define(version: 2019_07_16_140737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,18 @@ ActiveRecord::Schema.define(version: 2019_07_16_135923) do
     t.boolean "status"
     t.index ["event_id"], name: "index_participations_on_event_id"
     t.index ["user_id"], name: "index_participations_on_user_id"
+  end
+
+  create_table "prestataires", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_prestataires_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_prestataires_on_reset_password_token", unique: true
   end
 
   create_table "status_assignations", force: :cascade do |t|
